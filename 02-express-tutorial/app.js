982aee8d9a63bac75483bbca6e4fdb0618e3d878
Aleksandrs Kuskoveckis-Danilovs <52258392+kuskoveckis@users.noreply.github.com>
2021-06-06 19:29:13 +0100

    api

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,49 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-// setup static and middleware
-app.use(express.static("./public"));
+const { products } = require("./data");
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//   adding to static assets
-//   SSR
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1><a href='/api/products'>products</a>");
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find((product) => product.id === Number(productID));
+
+  if (!singleProduct) {
+    return res.status(404).send("Product doesn't exist");
+  }
+
+  res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched your search");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
