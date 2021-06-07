844fe677006374be8682a4264cf0a1ff155c96cf
Aleksandrs Kuskoveckis-Danilovs <52258392+kuskoveckis@users.noreply.github.com>
2021-06-07 19:46:07 +0100

    middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,49 +1,20 @@
 const express = require("express");
 const app = express();
 
-const { products } = require("./data");
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home page</h1><a href='/api/products'>products</a>");
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => product.id === Number(productID));
-
-  if (!singleProduct) {
-    return res.status(404).send("Product doesn't exist");
-  }
-
-  res.json(singleProduct);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send("no products matched your search");
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send("POST");
 });
 
 app.listen(5000, () => {
