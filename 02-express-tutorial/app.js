b22c3cf31e5af112d21e333a1dd2bf19a325ace8
Aleksandrs Kuskoveckis-Danilovs <52258392+kuskoveckis@users.noreply.github.com>
2021-06-10 20:24:49 +0100

    methods,router,controllers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express();
 
-let { people } = require("./data");
+const people = require("./routes/people");
+
+const auth = require("./routes/auth");
 
 // static assets
 app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+app.use("/api/people", people);
 
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  res.send("POST");
-});
+app.use("/login", auth);
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
